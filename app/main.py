c8b308d757d8d82d6049cf9b0eb24220b4c715e7
wonjjani <wonjjani@gmail.com>
2025-09-04 10:30:47 +0900

    haha: test ci/cd

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
+# app/main.py
 from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"ok": True, "service": "devops-lab", "version": "v2"}
+    return {"ok": True, "service": "devops-lab", "version": "v3"} 
